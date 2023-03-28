6a7c5ecaa3ca64cce157822e735078b8c4d7b2a0
Daniela Bustamante <96563765+DanielaBustamante@users.noreply.github.com>
2023-03-28 12:44:04 -0300

    Update Punto4.m

--- Punto4.m
+++ Punto4.m
@@ -1,1 +1,16 @@
+# Resolver los siguientes sistemas de forma matricial:
+# Si no es posible resolver alguno de ellos, mencionar por qué. Justificar. 
+# Efectuar un cambio en algún coeficiente para salvar esto y calcular. 
+# Justificar el cambio. 
 
+# A, C, C y D son un sistema
+A=-2+4.*X.^2-7.*X.^3+5.*X.^4;
+B=5+4.*X.^1-1.*X.^3;
+C=-1+6.*X.^1+4.*X.^2-6.*X.^3+8.*X.^4;
+D=-9-2.*X.^1+2.*X.^2+6.*X.^3+9.*X.^4;
+
+# E, F, G y H son un sistema
+E=-8+2.*X.^1+4.*X.^2-7.*X.^3+5.*X.^4;
+F=-3+4.*X.^1-1.*X.^3;
+G=4+6.*X.^1+4.*X.^2-6.*X.^3+8.*X.^4;
+H=7-2.*X.^1-4.*X.^2+7.*X.^3-5.*X.^4;
