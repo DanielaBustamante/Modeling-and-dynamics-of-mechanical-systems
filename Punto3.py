18aa8d833dc8f3ea238e1572887c2c2065211d35
Daniela Bustamante <96563765+DanielaBustamante@users.noreply.github.com>
2023-03-28 12:49:43 -0300

    Update Punto3.py

--- Punto3.py
+++ Punto3.py
@@ -1,1 +1,8 @@
+#Sea la siguiente grilla con los valores de temperatura en grados centígrados de una placa en el instante t=0, 
+# siendo los valores x e y distancias dadas en metros x 10-2.
 
+# a) Obtener el grafico de curva de nivel correspondiente a dicha grilla. 
+# Dar colores a cada nivel y mostrar la barra de colores de referencia, colocar título y etiquetas en los ejes. 
+
+# b) Obtener los siguientes gráficos de temperatura en grados centígrados en función de la distancia en metros, 
+# colocar título y etiquetas en los ejes.   b1: Diagonal x=y  b2: Línea y=5  b3: Línea x=5
