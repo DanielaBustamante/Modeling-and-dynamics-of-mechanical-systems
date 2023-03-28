4ae7ab000b43e2fa770b3e1626dd3d71e1b87283
Daniela Bustamante <96563765+DanielaBustamante@users.noreply.github.com>
2023-03-28 13:21:12 -0300

    Update Punto2.m

--- Punto2.m
+++ Punto2.m
@@ -1,4 +1,4 @@
-# Sean las siguientes matrices:
+% Sean las siguientes matrices:
 
 A=[0,4,-7,5;4,0,-1,0;6,4,-6,8;-2,2,6,9]
 
@@ -6,20 +6,20 @@ B=[2,4,-7,5;4,0,-1,0;6,4,-6,8;-2,-4,7,-5]
 
 C=[2,4,-7,0;4,3,-1,0;6,4,-6,0;-2,2,6,0]
 
-# a) Obtener el determinante de cada matriz. 
-# ¿Qué condición se debe dar para que en una matriz exista el determinante? 
-# Con el resultado obtenido, ¿Es posible obtener la matriz inversa?  
+% a) Obtener el determinante de cada matriz. 
+% ¿Qué condición se debe dar para que en una matriz exista el determinante? 
+% Con el resultado obtenido, ¿Es posible obtener la matriz inversa?  
 
-# b) Si no es posible, ¿qué habría que modificar para que sea posible? Si es así modificarlo. Justificar.  
+% b) Si no es posible, ¿qué habría que modificar para que sea posible? Si es así modificarlo. Justificar.  
 
-# c) Calcular la traspuesta y la inversa de cada matriz.   
+% c) Calcular la traspuesta y la inversa de cada matriz.   
 
-# d) Obtener los siguientes vectores fila de cada matriz:   
-# d1=[diagonal principal]  
-# d2=[fila 3]  
-# d3=[columna 3]  
+% d) Obtener los siguientes vectores fila de cada matriz:   
+% d1=[diagonal principal]  
+% d2=[fila 3]  
+% d3=[columna 3]  
 
-# e) Obtener las siguientes matrices de cada matriz:   
-# e1=[filas 1 y 2 x columnas 2 y 3]  
-# e2=[filas 3 y 4 x columnas 2 y 3]  
-# e3=[columnas 1 y 4 x filas 3 y 4 ] 
+% e) Obtener las siguientes matrices de cada matriz:   
+% e1=[filas 1 y 2 x columnas 2 y 3]  
+% e2=[filas 3 y 4 x columnas 2 y 3]  
+% e3=[columnas 1 y 4 x filas 3 y 4 ] 
