1fbde2cdaceb3d3e7c3c1ed96ece1cdd0eebe640
Daniela Bustamante <daniela.annabella05@gmail.com>
2023-03-31 12:54:44 -0300

    cambiospunto3

--- Trabajo N°1/Punto3.py
+++ Trabajo N°1/Punto3.py
@@ -4,34 +4,49 @@
 # a) Obtener el grafico de curva de nivel correspondiente a dicha grilla. 
 # Dar colores a cada nivel y mostrar la barra de colores de referencia, colocar título y etiquetas en los ejes. 
 
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import griddata
 
 # Leer los datos del archivo CSV
-df = pd.read_csv('Trabajo N°1/grilla_temperaturas.csv')
+data = np.genfromtxt('Trabajo N°1/grilla_temperaturas.csv', delimiter=',')
 
-# Convertir los datos en una matriz numpy
-data = df.values
+# Extrae las columnas x e y de los datos leídos
+x = data[:, 0]
+y = data[:, 1]
 
-# Obtener los valores de x, y y z
-x = data[:,0]
-y = data[:,1]
-z = data[:,2]
+# Extrae la columna de los valores z de los datos leídos
+z = data[:, 2]
 
-# Definir los valores de x y y para la cuadrícula
-xi = np.linspace(x.min(), x.max(), 100)
-yi = np.linspace(y.min(), y.max(), 100)
+# Identifica y elimina los valores faltantes en los datos utilizando la función isnan de numpy
+mask = ~(np.isnan(x) | np.isnan(y) | np.isnan(z))
+x = x[mask]
+y = y[mask]
+z = z[mask]
 
-# Crear una cuadrícula de coordenadas xi y yi
-X, Y = np.meshgrid(xi, yi)
+# Crea una cuadrícula de coordenadas xi y yi utilizando la función meshgrid de numpy
+xi = np.linspace(0, 10, 101)
+yi = np.linspace(0, 10, 101)
+xi, yi = np.meshgrid(xi, yi)
 
-# Obtener los valores de z para cada par de coordenadas xi, yi
-Z = griddata((x, y), z, (X, Y), method='linear')
+# Interpola los valores de z para las coordenadas xi e yi utilizando la función griddata de scipy
+zi = griddata((x, y), z, (xi, yi), method='linear')
 
-# Crear un gráfico de curvas de nivel
-plt.contour(X, Y, Z)
+# Crea el gráfico de curvas de nivel utilizando la función contour de matplotlib
+contour = plt.contour(xi, yi, zi, levels=10)
+
+# Establece los valores y etiquetas para los ticks en los ejes x e y utilizando las funciones xticks y yticks
+plt.xticks(np.arange(0, 11, 2))
+plt.yticks(np.arange(0, 11, 2))
+
+# Agrega una etiqueta para el eje x y y utilizando las funciones xlabel y ylabel
+plt.xlabel('x')
+plt.ylabel('y')
+
+# Agrega los valores de z como etiquetas de texto para cada curva de nivel
+plt.clabel(contour, inline=True, fontsize=8)
+
+# Muestra el gráfico
 plt.show()
 
 # b) Obtener los siguientes gráficos de temperatura en grados centígrados en función de la distancia en metros, 
